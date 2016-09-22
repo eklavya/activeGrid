a3f2a8d51e7f4937ec88cfc9f1a8ba438c69ea62
babjik <babji.kundateeri@gmail.com>
2016-09-22 19:16:46 +0530

    added code for neo4j wrapper

--- src/main/scala/com/imaginea/activegrid/core/dao/ImageInfoDAO.scala
+++ src/main/scala/com/imaginea/activegrid/core/dao/ImageInfoDAO.scala
@@ -1,18 +1,31 @@
 package com.imaginea.activegrid.core.dao
 
+import java.util.UUID
+
 import com.imaginea.activegrid.core.models.ImageInfo
 import com.typesafe.scalalogging.Logger
+import eu.fakod.neo4jscala.{EmbeddedGraphDatabaseServiceProvider, Neo4jWrapper}
 import org.slf4j.LoggerFactory
 
 
 /**
   * Created by babjik on 22/9/16.
   */
-class ImageInfoDAO  {
+class ImageInfoDAO extends Neo4jWrapper with EmbeddedGraphDatabaseServiceProvider{
   val logger = Logger(LoggerFactory.getLogger(getClass.getName))
+  val label = "ImageInfo"
+
+  def neo4jStoreDir = "./graphdb/activegriddb"
 
   def saveImage(image: ImageInfo): ImageInfo = {
     logger.debug(s"saving image ${image}")
+
+    withTx { implicit  neo =>
+      logger.debug(s"label to be created is ${label}")
+      val start = createNode(label)
+
+      logger.info(s"created node with label ${start.labels}")
+    }
     image
   }
 
