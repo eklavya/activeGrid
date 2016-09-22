74b8ff3bd53942ca9247f37f34b853e2a6fce8ec
babjik <babji.kundateeri@gmail.com>
2016-09-22 10:59:36 +0530

    added .idea/ in gitignore

--- src/main/scala/com/imaginea/activegrid/Main.scala
+++ src/main/scala/com/imaginea/activegrid/Main.scala
@@ -6,7 +6,6 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
---
+++ src/main/scala/com/imaginea/activegrid/core/utils/JsonSupport.scala
@@ -0,0 +1,8 @@
+package com.imaginea.activegrid.core.utils
+
+/**
+  * Created by babjik on 22/9/16.
+  */
+class JsonSupport {
+
+}
