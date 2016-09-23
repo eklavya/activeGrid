7462bfecb0886b1a9418c8ef1ed1d39f9aa6c2ba
sivag <siva.gollu@imaginea.com>
2016-09-23 10:49:04 +0530

    updated code for graph db

--- src/main/scala/com/activegrid/Utils.scala
+++ src/main/scala/com/activegrid/Utils.scala
@@ -1,6 +1,9 @@
 package com.activegrid
 
 import scala.concurrent.Future
+import akka.http.scaladsl.marshallers.sprayjson
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+
 import scala.concurrent.ExecutionContext.Implicits.global;
 
 
@@ -8,7 +11,8 @@ class Utils {
 
   def persistSoftwareDetails(software: Software): Future[Software] = Future {
     //Need to write neo4j code here
-    Software("1","Ubuntu","Linux") // Fake response
+    Software("1","""Ubuntu""","Linux") // Fake response
+
 
   }
 
