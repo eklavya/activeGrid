45f3e2fc0026ce134a261894239e48881e5c7007
naveed050 <shareef.naveed@gmail.com>
2016-09-26 15:41:44 +0530

    added few modifications to Catalog Service

--- src/main/scala/com/activegrid/model/ImageInfo.scala
+++ src/main/scala/com/activegrid/model/ImageInfo.scala
@@ -3,5 +3,19 @@ package com.activegrid.model
 /**
   * Created by shareefn on 22/9/16.
   */
-case class ImageInfo(imageId:String,imageType:String,lastUpdatedBy:String)
+case class ImageInfo(imageId : String,
+                     state : String,
+                     ownerId  :String,
+                     publicValue :Boolean,
+                     architecture  :String,
+                     imageType  :String,
+                     platform  :String,
+                     imageOwnerAlias  :String,
+                     name  :String,
+                     description  :String,
+                     rootDeviceType  :String,
+                     rootDeviceName  :String,
+                     version  :String) extends BaseEntity
+
+
 
