2da3416a421d9dbb9918d5c206de11a8f8f400ce
Sampath Rachakonda <sampath.rachakonda@imaginea.com>
2016-09-27 16:07:43 +0530

    Catalog Service RESTFUL Services for Softwares

--- src/main/scala/com/activegrid/model/Software.scala
+++ src/main/scala/com/activegrid/model/Software.scala
@@ -3,4 +3,4 @@ package com.activegrid.model
 /**
   * Created by sampathr on 22/9/16.
   */
-case class Software(version: String, name: String, provider: String, downloadURL: String, port: String, processName: String, discoverApplications: String)
+case class Software(softwareId: Long, createdAt: String, createdBy: String, lastUpdatedAt: String, lastUpdatedBy: String, version: String, name: String, provider: String, downloadURL: String, port: String, processName: String, discoverApplications: String) extends BaseEntity
---
+++ src/main/scala/com/activegrid/model/Test.scala
@@ -0,0 +1,6 @@
+package com.activegrid.model
+
+/**
+  * Created by sampathr on 27/9/16.
+  */
+case class Test(processNames:List[String])extends BaseEntity
