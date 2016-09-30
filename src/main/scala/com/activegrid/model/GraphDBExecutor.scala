c22d5ebec6827d67cfe06e5f3716044225cc8391
Sampath Rachakonda <sampath.rachakonda@imaginea.com>
2016-09-30 11:36:07 +0530

    Trying to write custom wrapper

--- src/main/scala/com/activegrid/model/GraphDBExecutor.scala
+++ src/main/scala/com/activegrid/model/GraphDBExecutor.scala
@@ -19,10 +19,38 @@ class GraphDBExecutor extends Neo4jWrapper with EmbeddedGraphDatabaseServiceProv
       val node = createNode(entity, label)(neo)
       //      println(s" new software ${node.getLabels}, id ${node.getId}")
       //      println(s" Software Name ${node.getProperty("name")}")
+      val fieldsInSite = entity.getClass.getDeclaredFields()
+      fieldsInSite.foreach(println)
+
+      val ClassOfList = classOf[List[String]]
+      val ClassOfInt = classOf[Int]
+      val ClassOfString = classOf[String]
+
+      fieldsInSite.foreach(field => {
+        println("-->" + field.getName + "<---" + field.getType)
+        field.setAccessible(true)
+        field.getType match {
+          case ClassOfList => {
+            //TO DO: need to get entity.instances in other way
+            // val listToArray = entity.instances.toArray
+            val list2Array = field.get(entity).asInstanceOf[scala.collection.immutable.List[String]].toArray
+            node.setProperty(field.getName,list2Array)
+          }
+          case ClassOfInt =>{
+            node.setProperty(field.getName,field.get(entity).asInstanceOf[Int])
+          }
+          case ClassOfString => {
+            node.setProperty(field.getName,field.get(entity).asInstanceOf[String])
+          }
+        }
+      })
       println("Test Run")
     }
+
+
     return Some(entity)
   }
+
 
   def getEntities[T: Manifest](label: String): Option[List[T]] = {
 
