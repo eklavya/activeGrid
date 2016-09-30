a93d32223492b79c33e824ffc57b8da2687cf2e2
nagulmeeras <nagulmeeras@PRINHYLTPAP1001.local>
2016-09-30 19:22:22 +0530

    made some changes and written app setting end points

--- src/main/scala/com/activegrid/services/AppSettingService.scala
+++ src/main/scala/com/activegrid/services/AppSettingService.scala
@@ -3,11 +3,11 @@ package com.activegrid.services
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.stream.ActorMaterializer
-import org.slf4j.LoggerFactory
 import akka.http.scaladsl.server.Directives._
-import com.activegrid.entities.{AppSettings, JsonSupport, Setting}
-import com.activegrid.repositories._
+import akka.stream.ActorMaterializer
+import com.activegrid.models.{AppSettingRepository, AppSettings, JsonSupport, Setting}
+import com.typesafe.config.ConfigFactory
+import org.slf4j.LoggerFactory
 
 
 object AppSettingService extends App with JsonSupport {
@@ -15,10 +15,27 @@ object AppSettingService extends App with JsonSupport {
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
+  implicit val config = ConfigFactory.load()
 
   val logger = LoggerFactory.getLogger(getClass)
   val appSettingRepository = new AppSettingRepository
 
+  val addAppSetting = post {
+    path("appsettings") {
+      entity(as[AppSettings]) {
+        appsetting => complete {
+          appSettingRepository.saveAppSettings(appsetting)
+        }
+      }
+    }
+  }
+  val getAppSettings = get {
+    path("appsettings") {
+      complete {
+        appSettingRepository.getAppSettings()
+      }
+    }
+  }
   val addSetting = post {
     path("settings") {
       entity(as[List[Setting]]) {
@@ -29,7 +46,7 @@ object AppSettingService extends App with JsonSupport {
     }
   }
 
-  val getAppSettings = path("settings") {
+  val getSettings = path("settings") {
     get {
       complete(appSettingRepository.getSettings())
     }
@@ -44,9 +61,9 @@ object AppSettingService extends App with JsonSupport {
     }
   }
 
-  val routes = addSetting ~ getAppSettings ~ deleteSettings
+  val routes = addAppSetting ~ getAppSettings ~ addSetting ~ getAppSettings ~ deleteSettings
 
-  Http().bindAndHandle(routes, "localhost", 9000)
+  Http().bindAndHandle(routes, config.getString("http.host"), config.getInt("http.port"))
 
   println("Server Running")
 
