a2af6eeff3196102f19a406bd7f25d8c3194f3df
nagulmeeras <nagulmeeras@PRINHYLTPAP1001.local>
2016-09-30 19:23:04 +0530

    made some changes and written app setting end points

---
+++ src/main/scala/com/activegrid/models/JsonSupport.scala
@@ -0,0 +1,27 @@
+package com.activegrid.models
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
+import spray.json.{DefaultJsonProtocol, JsString, JsValue, JsonFormat}
+
+import scala.util.Try
+
+/**
+  * Created by nagulmeeras on 28/09/16.
+  */
+trait JsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
+  implicit val settingFormat = jsonFormat2(Setting.apply)
+
+  implicit object DateFormate extends JsonFormat[DateTime] {
+    val parser = ISODateTimeFormat.dateOptionalTimeParser()
+    override def write(obj: DateTime): JsString = JsString(ISODateTimeFormat.basicDateTime.print(obj))
+
+    override def read(json : JsValue): DateTime = json match {
+      case JsString(json) => parser.parseDateTime(json)
+      case _ => throw new Exception("Unable to Deserialize")
+    }
+  }
+
+  implicit val appSettingsFormat = jsonFormat5(AppSettings.apply)
+}
