18fff74e56cf466e558f0112cc58d77b53ff2e7d
nagulmeeras <nagulmeeras@PRINHYLTPAP1001.local>
2016-10-04 19:02:55 +0530

    Wriiten toGraph and fromGraph implementaions for saving and retrieving

--- src/main/scala/com/activegrid/models/AppSettings.scala
+++ src/main/scala/com/activegrid/models/AppSettings.scala
@@ -1,7 +1,8 @@
 package com.activegrid.models
 
+import org.neo4j.graphdb.{Direction, Node, Relationship, RelationshipType}
 
-import org.joda.time.DateTime
+import scala.collection.mutable
 
 
 /**
@@ -9,7 +10,92 @@ import org.joda.time.DateTime
   */
 
 case class AppSettings(id: Int,
-                       createdAt: DateTime,
+                       createdAt: String,
                        createdBy: String,
-                       lastUpdatedAt: DateTime,
-                       lastUpdatedBy: String) extends BaseEntity with Neo4jRep[AppSettings]
+                       lastUpdatedAt: String,
+                       lastUpdatedBy: String,
+                       list: List[Setting]) extends BaseEntity
+
+object Implicits {
+  val repo = Neo4JRepo
+  implicit class AppSettingsImpl(entity: AppSettings) extends Neo4jRep[AppSettings] {
+    val labelName = "AppSettings"
+    override def toGraph(): Node = {
+
+      repo.withTx { neo =>
+        val node: Node = repo.createNode(labelName)(neo)
+        node.setProperty("id", entity.id)
+        node.setProperty("createdAt", entity.createdAt)
+        node.setProperty("createdBy", entity.createdBy)
+        node.setProperty("lastUpdatedAt", entity.lastUpdatedAt)
+        node.setProperty("lastUpdatedBy", entity.lastUpdatedBy)
+        entity.list.foreach {
+          setting =>
+              val childNode = setting.toGraph()
+              createRelationShip(node, childNode, "Has")
+        }
+        node
+      }
+    }
+
+    override def fromGraph(): AppSettings = {
+      var appSettings:AppSettings = null
+      repo.withTx {
+        neo =>
+          println("Coming executing ")
+          val nodes = repo.getAllNodesWithLabel("AppSettings")(neo)
+
+          nodes.foreach {
+            node =>
+              val relationships = node.getRelationships
+              val relationIterator = relationships.iterator()
+              val settingsList: scala.collection.mutable.MutableList[Setting] = mutable.MutableList()
+              while (relationIterator.hasNext){
+                val relationship = relationIterator.next()
+                val endNode = relationship.getEndNode;
+                val setting : Setting = new Setting(endNode.getProperty("key").toString, endNode.getProperty("value").toString)
+                settingsList += (setting)
+              }
+
+              appSettings = new AppSettings(Int.unbox(node.getProperty("id")),node.getProperty("createdAt").toString,node.getProperty("createdBy").toString
+                ,node.getProperty("lastUpdatedAt").toString,node.getProperty("lastUpdatedBy").toString,settingsList.toList)
+
+          }
+      }
+
+      appSettings
+    }
+    def getAppSettingNode():Node = {
+      repo.withTx{
+        neo => val nodes = repo.getAllNodesWithLabel(labelName)(neo)
+          nodes.head
+      }
+    }
+  }
+
+  implicit class SettingsImpl(setting : Setting) extends Neo4jRep[Setting]{
+    val labelName = "Setting"
+    override def toGraph(): Node = {
+      repo.withTx{
+        neo =>
+          val node = repo.createNode(labelName)(neo)
+          node.setProperty("key", setting.key)
+          node.setProperty("value", setting.value)
+          node
+      }
+    }
+
+  }
+
+  implicit def createRelationShip(parentNode : Node, childNode : Node , relationship : String): Unit ={
+    repo.withTx{
+      neo =>
+        parentNode.createRelationshipTo(childNode , new RelationshipType {
+          override def name(): String = relationship
+        })
+    }
+  }
+
+}
+
+
---
+++ src/main/scala/com/activegrid/models/Neo4JRepo.scala
@@ -0,0 +1,8 @@
+package com.activegrid.models
+
+/**
+  * Created by nagulmeeras on 03/10/16.
+  */
+object Neo4JRepo extends Neo4jRepository{
+
+}
