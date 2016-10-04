7c90e9a24f62c152476d2bf892af39e31902ef14
nagulmeeras <nagulmeeras@PRINHYLTPAP1001.local>
2016-10-04 19:02:55 +0530

    Wriiten toGraph and fromGraph implementaions for saving and retrieving

--- src/main/scala/com/activegrid/models/Neo4jRepository.scala
+++ src/main/scala/com/activegrid/models/Neo4jRepository.scala
@@ -8,10 +8,11 @@ import org.slf4j.LoggerFactory
   * Created by nagulmeeras on 28/09/16.
   */
 trait Neo4jRepository extends Neo4jWrapper with EmbeddedGraphDatabaseServiceProvider {
-  val logger = LoggerFactory.getLogger(getClass)
+  override def neo4jStoreDir = "./activeGridDB"
+  //val logger = LoggerFactory.getLogger(getClass)
 
   def persistEntity[T <: AnyRef](entity: T, labelName: String): Long = withTx {
-    logger.info(s"Executing $getClass :: persistEntity ")
+    //logger.info(s"Executing $getClass :: persistEntity ")
     neo => val node: Node = createNode(entity, labelName)(neo)
       //      entity.getClass.getDeclaredFields.foreach{feild =>
       //        feild.getType.isInstance(List[T]) match {
@@ -20,30 +21,34 @@ trait Neo4jRepository extends Neo4jWrapper with EmbeddedGraphDatabaseServiceProv
       //
       //
       //      }}
-      logger.info("returning from persistEntity method")
+     // logger.info("returning from persistEntity method")
       node.getId
   }
 
   def getEntityById[T: Manifest](id: Long, labelName: String): T = withTx {
-    logger.info(s"Executing $getClass :: getEntity ")
+    //logger.info(s"Executing $getClass :: getEntity ")
     neo => val node: Node = getNodeById(id)(neo)
       node.toCC[T].get
   }
 
   def getEntitiesByLabel[T: Manifest](labelName: String): List[T] = withTx {
-    logger.info(s"Executing $getClass :: getEntitiesByLabel ")
+    //logger.info(s"Executing $getClass :: getEntitiesByLabel ")
     neo => val nodes = getAllNodesWithLabel(labelName)(neo)
       val entities = nodes.map(_.toCC[T].get).toList
       entities
   }
 
   def deleteEntity(labelName: String, propertyName: String, propertyValue: String): Boolean = withTx {
-    logger.info(s"Executing $getClass :: deleteEntity ")
+    //logger.info(s"Executing $getClass :: deleteEntity ")
     neo => val nodes = findNodesByLabelAndProperty(labelName, propertyName, propertyValue)(neo)
       nodes.foreach(node => node.delete())
       true
   }
 
+  def createEmptyNode(label : String): Node = withTx{
+    neo => val node = createNode(label) (neo)
+      node
+  }
   def persistRelationShip(parentId: Long, childId: Long, relation: String): Unit = {
     //val cQl = s"MATCH (user:Users {id: '$parentId'}) FOREACH (id in [$childId] | CREATE (user)-[:$relation]->(:Users {name:name}))"
   }
