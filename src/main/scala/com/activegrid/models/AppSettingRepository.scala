7560377319eb80c12b0980c42b72eb555b77be36
nagulmeeras <nagulmeeras@PRINHYLTPAP1001.local>
2016-10-04 19:02:55 +0530

    Wriiten toGraph and fromGraph implementaions for saving and retrieving

--- src/main/scala/com/activegrid/models/AppSettingRepository.scala
+++ src/main/scala/com/activegrid/models/AppSettingRepository.scala
@@ -1,7 +1,7 @@
 package com.activegrid.models
 
-import java.util.Date
 
+import com.activegrid.models.Implicits.{AppSettingsImpl, SettingsImpl}
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -10,50 +10,56 @@ import scala.concurrent.{ExecutionContext, Future}
   * Created by nagulmeeras on 27/09/16.
   */
 
-class AppSettingRepository(implicit executionContext: ExecutionContext) extends Neo4jRepository {
+class AppSettingRepository(implicit executionContext: ExecutionContext)  {
 
-  override val logger = LoggerFactory.getLogger(getClass)
+  val logger = LoggerFactory.getLogger(getClass)
   val settingsLabelName = "Settings"
   val appSettingsLabelName = "AppSettings"
 
-  override def neo4jStoreDir = "./activeGridDB"
 
   def saveAppSettings(appSettings: AppSettings): Future[AppSettings] = Future {
     logger.info(s"Executing $getClass :: saveAppSettings ")
     logger.info("AppSettings : " + appSettings)
-    val id: Long = persistEntity(appSettings, appSettingsLabelName)
-    logger.info(s" saved the app Settings with ID:$id ")
+    val appSettingImpl = new AppSettingsImpl(appSettings)
+    appSettingImpl.toGraph()
+    //val id: Long = persistEntity(appSettings, appSettingsLabelName)
+    //logger.info(s" saved the app Settings with ID:$id ")
     appSettings
   }
 
-  def getAppSettings(): Future[List[AppSettings]] = Future {
+  def getAppSettings(): Future[AppSettings] = Future {
     logger.info(s"Executing $getClass getAppSettings")
-    val appSettings = getEntitiesByLabel(appSettingsLabelName)
-    appSettings
+    val appSettingsImpl = new AppSettingsImpl(null)
+    val appSettings1 : AppSettings =  appSettingsImpl.fromGraph()
+    appSettings1
   }
 
 
-  def saveSetting(settings: List[Setting]): Future[Setting] = Future {
+  def saveSetting(setting :Setting): Future[Setting] = Future {
     logger.info(s"Executing $getClass ::saveSetting")
-    settings.foreach(setting => persistEntity(setting, settingsLabelName))
-    settings.head
+    val appSettingsImpl = new AppSettingsImpl(null)
+    val settingsImpl = new SettingsImpl(setting)
+    val settingNode = settingsImpl.toGraph()
+    val appSettingsNode = appSettingsImpl.getAppSettingNode()
+    Implicits.createRelationShip(appSettingsNode,settingNode,"Has")
+    setting
   }
 
-  def getSettings(): Future[List[Setting]] = Future {
-    logger.info(s"Executing $getClass ::getSettings")
-    val settings = getEntitiesByLabel[Setting](settingsLabelName)
-    settings
-  }
+//  def getSettings(): Future[List[Setting]] = Future {
+//    logger.info(s"Executing $getClass ::getSettings")
+//    val settings = getEntitiesByLabel[Setting](settingsLabelName)
+//    settings
+//  }
 
-  def deleteSettings(settingNames: List[Setting]): Unit = {
-    logger.info("Executing deleteSettings")
-    settingNames.foreach(setting => {
-      logger.info(s"  $setting " + setting.key)
-      deleteEntity(settingsLabelName, "key", setting.key)
-    }
-    )
-    logger.info("Deleted settings")
-  }
+//  def deleteSettings(settingNames: List[Setting]): Unit = {
+//    logger.info("Executing deleteSettings")
+//    settingNames.foreach(setting => {
+//      logger.info(s"  $setting " + setting.key)
+//      deleteEntity(settingsLabelName, "key", setting.key)
+//    }
+//    )
+//    logger.info("Deleted settings")
+//  }
 
 
 }
