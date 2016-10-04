928743a2ffdb3480474d49800d88542a1519aecf
nagulmeeras <nagulmeeras@PRINHYLTPAP1001.local>
2016-10-04 19:02:55 +0530

    Wriiten toGraph and fromGraph implementaions for saving and retrieving

--- src/main/scala/com/activegrid/models/Neo4jRep.scala
+++ src/main/scala/com/activegrid/models/Neo4jRep.scala
@@ -5,7 +5,7 @@ import org.neo4j.graphdb.Node
 /**
   * Created by nagulmeeras on 30/09/16.
   */
-trait Neo4jRep[T <: BaseEntity] {
-  def toGraph(entity : T) : Node = ???
+trait Neo4jRep[T <: BaseEntity]{
+  def toGraph() : Node = ???
   def fromGraph():T = ???
 }
