f997366abe0f0adbb14a923295ca152e9985f50e
Sampath Rachakonda <sampath.rachakonda@imaginea.com>
2016-10-05 17:04:52 +0530

    Catalog Service 6 API calls

--- src/main/scala/com/activegrid/model/BaseEntity.scala
+++ src/main/scala/com/activegrid/model/BaseEntity.scala
@@ -4,6 +4,5 @@ package com.activegrid.model
   * Created by sampathr on 22/9/16.
   */
 trait BaseEntity {
-
-
+  val id: Option[Long]
 }
--- src/main/scala/com/activegrid/model/Page.scala
+++ src/main/scala/com/activegrid/model/Page.scala
@@ -4,7 +4,6 @@ package com.activegrid.model
   * Created by sampathr on 22/9/16.
   */
 case class Page[T](startIndex:Int, count:Int, totalObjects: Int, objects:List[T]) {
- // def this(objects:List[T]) = this(0,objects.size, objects.size, objects)
 }
 
 
