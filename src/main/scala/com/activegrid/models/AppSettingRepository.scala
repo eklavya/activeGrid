0478ca3fed89105f26f08cb2632be6cb5c4a2ad4
nagulmeeras <nagulmeera.shaik@imaginea.com>
2016-10-07 15:34:54 +0530

    Changes made according to the design in neo4j repositories

--- src/main/scala/com/activegrid/models/AppSettingRepository.scala
+++ src/main/scala/com/activegrid/models/AppSettingRepository.scala
@@ -1,7 +1,7 @@
 package com.activegrid.models
 
 
-import com.activegrid.models.Implicits.{AppSettingsImpl, SettingsImpl}
+import com.activegrid.models.AppSettings.AppSettingsImpl
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -10,47 +10,42 @@ import scala.concurrent.{ExecutionContext, Future}
   * Created by nagulmeeras on 27/09/16.
   */
 
-class AppSettingRepository(implicit executionContext: ExecutionContext)  {
+class AppSettingRepository(implicit executionContext: ExecutionContext) {
 
   val logger = LoggerFactory.getLogger(getClass)
-  val settingsLabelName = "Settings"
-  val appSettingsLabelName = "AppSettings"
 
 
-  def saveAppSettings(appSettings: AppSettings): Future[AppSettings] = Future {
+  def saveAppSettings(appSettings: AppSettings): Future[Unit] = Future{
     logger.info(s"Executing $getClass :: saveAppSettings ")
     logger.info("AppSettings : " + appSettings)
-    val appSettingImpl = new AppSettingsImpl(appSettings)
-    appSettingImpl.toGraph()
-    appSettings
+    appSettings.toNeo4jGraph()
   }
 
   def getAppSettings(): Future[AppSettings] = Future {
     logger.info(s"Executing $getClass getAppSettings")
-    val appSettingsImpl = new AppSettingsImpl(null)
-    val appSettings : AppSettings =  appSettingsImpl.fromGraph()
+    val appSettings = new AppSettings(Some(0), null, null).fromNeo4jGraph();
     appSettings
   }
 
 
-  def saveSetting(setting :Map[String,String]): Unit ={
+  def saveSetting(setting: Map[String, String]): Unit = {
     logger.info(s"Executing $getClass ::saveSetting")
     val appSettingsImpl = new AppSettingsImpl(null)
-    val appSettings = appSettingsImpl.updateAppSettings(setting,"Has_Settings")
-
+    val appSettings = appSettingsImpl.updateAppSettings(setting, "Has_Settings")
   }
 
-  def getSettings(): Future[AppSettings] ={
+  def getSettings(): Future[AppSettings] = {
     logger.info(s"Executing $getClass ::getSettings")
     val settings = getAppSettings()
     settings
   }
 
-  def deleteSettings(settingNames: List[String]): Unit = {
+  def deleteSettings(settingNames: List[String]): Future[Boolean] = Future {
     logger.info("Executing deleteSettings")
     val appSettingsImpl = new AppSettingsImpl(null)
     appSettingsImpl.deleteSettings(settingNames, "Has_Settings")
     logger.info("Deleted settings")
+    true
   }
 
 
