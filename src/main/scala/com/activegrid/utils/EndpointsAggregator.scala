70c482b6535f099ebcc80005110e7952952acfb8
sivag <siva.gollu@imaginea.com>
2016-10-07 18:47:23 +0530

    formatting code and neo4j package name letter-case changed

--- src/main/scala/com/activegrid/utils/EndpointsAggregator.scala
+++ src/main/scala/com/activegrid/utils/EndpointsAggregator.scala
@@ -1,7 +1,7 @@
 package com.activegrid.utils
 
-import com.activegrid.services.{AppSettingsService, LogConfigUpdaterService}
 import akka.http.scaladsl.server.Directives._
+import com.activegrid.services.{AppSettingsService, LogConfigUpdaterService}
 
 /**
   * Created by sivag on 27/9/16.
