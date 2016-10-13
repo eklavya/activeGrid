379d7cebba569f6e5b64b3af5f7036b82766b0f4
naveed050 <shareef.naveed@gmail.com>
2016-10-13 15:52:14 +0530

    Catalog Service after making changes

--- src/main/scala/com/activegrid/model/InstanceUser.scala
+++ src/main/scala/com/activegrid/model/InstanceUser.scala
@@ -10,32 +10,33 @@ import org.neo4j.graphdb.Node
 case class InstanceUser(override val id: Option[Long],userName: String, publicKeys: List[String])  extends BaseEntity
 
 object InstanceUser{
+
   implicit class InstanceUserImpl(instanceUser: InstanceUser) extends Neo4jRep[InstanceUser]{
 
     override def toNeo4jGraph(entity: InstanceUser): Option[Node] = {
 
-      val label: String = "InstanceUser"
+      val label = "InstanceUser"
 
-      val mapPrimitives : Map[String, Any] = Map("userName" -> entity.userName, "publicKeys" -> entity.publicKeys.toArray)
+      val mapPrimitives  = Map("userName" -> entity.userName, "publicKeys" -> entity.publicKeys.toArray)
 
-      val node: Option[Node] = GraphDBExecutor.createGraphNodeWithPrimitives[TestImplicit](label, mapPrimitives)
+      val node: Option[Node] = GraphDBExecutor.createGraphNodeWithPrimitives[InstanceUser](label, mapPrimitives)
 
       node
+
     }
 
-    override def fromNeo4jGraph(nodeId: Long): Option[InstanceUser] = {
+    override def fromNeo4jGraph(nodeId: Long): InstanceUser = {
 
-      val listOfKeys: List[String] = List("userName","publicKeys")
+      val listOfKeys = List("userName","publicKeys")
+      val propertyValues = GraphDBExecutor.getGraphProperties(nodeId,listOfKeys)
+      val userName = propertyValues.get("userName").get.toString
+      val publicKeys  = propertyValues.get("publicKeys").get.asInstanceOf[Array[String]].toList
 
-      val propertyValues: Map[String,Any] = GraphDBExecutor.getGraphProperties(nodeId,listOfKeys)
-      val userName: String = propertyValues.get("userName").get.toString
-      val publicKeys: List[String] = propertyValues.get("publicKeys").get.asInstanceOf[Array[String]].toList
+      InstanceUser(Some(nodeId), userName,publicKeys)
 
-      Some(InstanceUser(Some(nodeId), userName,publicKeys))
     }
 
   }
-
 
 }
 
--- src/main/scala/com/activegrid/model/SSHAccessInfo.scala
+++ src/main/scala/com/activegrid/model/SSHAccessInfo.scala
@@ -9,19 +9,18 @@ import org.neo4j.graphdb.Node
 case class SSHAccessInfo(override val id: Option[Long],keyPair : KeyPairInfo, userName: String, port: Int)  extends BaseEntity
 
 object SSHAccessInfo{
+
   implicit class SSHAccessInfoImpl(sshAccessInfo: SSHAccessInfo) extends Neo4jRep[SSHAccessInfo] {
+
     override def toNeo4jGraph(entity: SSHAccessInfo): Option[Node] = {
 
+      val label = "SSHAccessInfo"
 
-      val label: String = "SSHAccessInfo"
-
-      val mapPrimitives : Map[String, Any] = Map("userName" -> entity.userName, "port" -> entity.port)
+      val mapPrimitives  = Map("userName" -> entity.userName, "port" -> entity.port)
 
       val node: Option[Node] = GraphDBExecutor.createGraphNodeWithPrimitives[SSHAccessInfo](label, mapPrimitives)
 
-
       val node2: Option[Node] = entity.keyPair.toNeo4jGraph(entity.keyPair)
-
 
       val relationship = "HAS_keyPair"
       GraphDBExecutor.setGraphRelationship(node,node2,relationship)
@@ -30,26 +29,24 @@ object SSHAccessInfo{
 
     }
 
-    override def fromNeo4jGraph(nodeId: Long): Option[SSHAccessInfo] = {
+    override def fromNeo4jGraph(nodeId: Long): SSHAccessInfo = {
 
-      val listOfKeys: List[String] = List("userName","port")
+      val listOfKeys  = List("userName","port")
 
-
-      val propertyValues: Map[String,Any] = GraphDBExecutor.getGraphProperties(nodeId,listOfKeys)
-      val userName: String = propertyValues.get("userName").get.toString
-      val port: Int = propertyValues.get("port").get.toString.toInt
-
+      val propertyValues = GraphDBExecutor.getGraphProperties(nodeId,listOfKeys)
+      val userName = propertyValues.get("userName").get.toString
+      val port  = propertyValues.get("port").get.toString.toInt
 
       val relationship = "HAS_keyPair"
       val childNodeId = GraphDBExecutor.getChildNodeId(nodeId,relationship)
 
       val keyPair:KeyPairInfo = null
-      val keyPairInfo: Option[KeyPairInfo] = keyPair.fromNeo4jGraph(childNodeId)
-      Some(SSHAccessInfo(Some(nodeId),keyPairInfo.get, userName, port))
+      val keyPairInfo: KeyPairInfo = keyPair.fromNeo4jGraph(childNodeId)
+      SSHAccessInfo(Some(nodeId),keyPairInfo, userName, port)
 
     }
-  }
 
+  }
 
 }
 
