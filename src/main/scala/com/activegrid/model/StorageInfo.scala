3a9a4f7f02fd37e67da95c9174adc8fbf3649e99
naveed050 <shareef.naveed@gmail.com>
2016-10-17 11:21:58 +0530

    made changes to create graph with optional entities for some fields

--- src/main/scala/com/activegrid/model/StorageInfo.scala
+++ src/main/scala/com/activegrid/model/StorageInfo.scala
@@ -6,36 +6,38 @@ import org.neo4j.graphdb.Node
 /**
   * Created by shareefn on 7/10/16.
   */
-case class StorageInfo(override val id: Option[Long],used: Double, total: Double) extends BaseEntity
+case class StorageInfo(override val id: Option[Long], used: Double, total: Double) extends BaseEntity
 
 object StorageInfo {
 
   implicit class StorageInfoImpl(storageInfo: StorageInfo) extends Neo4jRep[StorageInfo] {
 
     override def toNeo4jGraph(entity: StorageInfo): Option[Node] = {
-
       val label = "StorageInfo"
-
       val mapPrimitives = Map("used" -> entity.used, "total" -> entity.total)
-
       val node = GraphDBExecutor.createGraphNodeWithPrimitives[StorageInfo](label, mapPrimitives)
 
       node
-
     }
 
-    override def fromNeo4jGraph(nodeId: Long): StorageInfo = {
-
-      val listOfKeys  = List("used","total")
-
-      val propertyValues = GraphDBExecutor.getGraphProperties(nodeId,listOfKeys)
-      val used: Double = propertyValues.get("used").get.toString.toDouble
-      val total : Double =  propertyValues.get("total").get.toString.toDouble
-
-      StorageInfo(Some(nodeId),used,total)
-
+    override def fromNeo4jGraph(id: Option[Long]): Option[StorageInfo] = {
+      StorageInfo.fromNeo4jGraph(id)
     }
 
+  }
+
+  def fromNeo4jGraph(id: Option[Long]): Option[StorageInfo] = {
+    id match {
+      case Some(nodeId) => {
+        val listOfKeys = List("used", "total")
+        val propertyValues = GraphDBExecutor.getGraphProperties(nodeId, listOfKeys)
+        val used: Double = propertyValues.get("used").get.toString.toDouble
+        val total: Double = propertyValues.get("total").get.toString.toDouble
+
+        Some(StorageInfo(Some(nodeId), used, total))
+      }
+      case None => None
+    }
   }
 
 }
