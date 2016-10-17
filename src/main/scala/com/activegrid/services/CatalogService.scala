88d4f2c35839183464491e06bfbacb3ee58397c1
naveed050 <shareef.naveed@gmail.com>
2016-10-17 13:08:50 +0530

    changed return type from list to Page

--- src/main/scala/com/activegrid/services/CatalogService.scala
+++ src/main/scala/com/activegrid/services/CatalogService.scala
@@ -9,44 +9,36 @@ import scala.concurrent.{ExecutionContext, Future}
   */
 class CatalogService(implicit val executionContext: ExecutionContext){
 
-  def getImages(): Future[List[ImageInfo]] = Future {
+  def getImages(): Future[Page[ImageInfo]] = Future {
 
     val label: String = "ImageInfo"
-
     val nodesList = GraphDBExecutor.getNodesByLabel(label)
     val imageInfoList = nodesList.map(node => ImageInfo.fromNeo4jGraph(Some(node.getId)).get)
 
-    imageInfoList
-
+    Page[ImageInfo](imageInfoList)
   }
 
   def buildImage(image:ImageInfo):Future[String] = Future {
 
-
     image.toNeo4jGraph(image)
 
     "Built ImageInfo Successfully"
-
   }
 
   def deleteImage(imageId:Long): Future[String] = Future {
 
-
     GraphDBExecutor.deleteEntity[ImageInfo](imageId)
 
     "Deleted image successfully"
-
   }
 
-  def getInstanceFlavor(siteId: Long): Future[List[InstanceFlavor]] = Future {
-
+  def getInstanceFlavor(siteId: Long): Future[Page[InstanceFlavor]] = Future {
 
     val site  = Site.fromNeo4jGraph(Some(siteId)).get
-
     val listOfInstances = site.instances
+    val listOfInstanceFlavors = listOfInstances.map(instance => InstanceFlavor(instance.instanceType.get, None, instance.memoryInfo.get.total,instance.rootDiskInfo.get.total))
 
-    listOfInstances.map(instance => InstanceFlavor(instance.instanceType.get, None, instance.memoryInfo.get.total,instance.rootDiskInfo.get.total))
-
+    Page[InstanceFlavor](listOfInstanceFlavors)
   }
 
 }
