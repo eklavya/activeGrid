5f57c77ceed16f42467ea5ffe6165a56612fa8b4
nagulmeeras <nagulmeera.shaik@imaginea.com>
2016-10-19 12:28:28 +0530

    Removed service layer and modified throwing exception by None

--- src/main/scala/com/activegrid/Main.scala
+++ src/main/scala/com/activegrid/Main.scala
@@ -7,8 +7,10 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.PathMatchers
 import akka.stream.ActorMaterializer
 import com.activegrid.models._
-import com.activegrid.services.{APMServerDetailsService, AppSettingService}
 import org.slf4j.LoggerFactory
+
+import scala.collection.mutable
+import scala.concurrent.Future
 
 
 object Main extends App with JsonSupport {
@@ -19,15 +21,15 @@ object Main extends App with JsonSupport {
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
-  val aPMService = new APMServerDetailsService
   val logLevelUpdater = new LogLevelUpdater
-  val appSettingRepository = new AppSettingService
 
   def appSettingServiceRoutes = post {
     path("appsettings") {
       entity(as[AppSettings]) {
         appsetting =>
-          onComplete(appSettingRepository.saveAppSettings(appsetting)) {
+          onComplete(Future {
+            appsetting.toNeo4jGraph()
+          }) {
             case util.Success(response) => complete(StatusCodes.OK, "Done")
             case util.Failure(exception) => {
               logger.error(s"Unable to save App Settings $exception")
@@ -38,9 +40,20 @@ object Main extends App with JsonSupport {
     }
   } ~ get {
     path("config") {
-      val appSettings = appSettingRepository.getAppSettings()
+      val nodeId = AppSettings.getAppSettingNode() match {
+        case Some(node) => Some(node.getId)
+        case None => None
+      }
+      val appSettings = Future {
+        AppSettings.fromNeo4jGraph(nodeId)
+      }
       onComplete(appSettings) {
-        case util.Success(response) => complete(StatusCodes.OK, response)
+        case util.Success(response) => {
+          response match {
+            case Some(appSettings) => complete(StatusCodes.OK, appSettings)
+            case None => complete(StatusCodes.OK, "Unable to get the App Settings")
+          }
+        }
         case util.Failure(exception) => {
           logger.error(s"Unable to get App Settings")
           complete(StatusCodes.BadRequest, "Unable to get App Settings")
@@ -52,8 +65,10 @@ object Main extends App with JsonSupport {
     path(PathMatchers.separateOnSlashes("config/settings")) {
       entity(as[Map[String, String]]) {
         setting =>
-          val resp = appSettingRepository.saveSetting(setting)
-          onComplete(resp) {
+          val response = Future {
+            AppSettings.updateAppSettings(setting, "Has_Settings")
+          }
+          onComplete(response) {
             case util.Success(response) => complete(StatusCodes.OK, "Done")
             case util.Failure(exception) => {
               logger.error(s"Unable to save the settings $exception")
@@ -64,7 +79,13 @@ object Main extends App with JsonSupport {
     }
   } ~ path(PathMatchers.separateOnSlashes("config/settings")) {
     get {
-      val appSettings = appSettingRepository.getSettings()
+      val nodeId = AppSettings.getAppSettingNode() match {
+        case Some(node) => Some(node.getId)
+        case None => None
+      }
+      val appSettings = Future {
+        AppSettings.fromNeo4jGraph(nodeId)
+      }
       onComplete(appSettings) {
         case util.Success(response) => complete(StatusCodes.OK, response)
         case util.Failure(exception) => {
@@ -76,7 +97,9 @@ object Main extends App with JsonSupport {
   } ~ path(PathMatchers.separateOnSlashes("config/settings")) {
     delete {
       entity(as[List[String]]) { list =>
-        val isDeleted = appSettingRepository.deleteSettings(list)
+        val isDeleted = Future {
+          AppSettings.deleteSettings(list, "Has_Settings")
+        }
         onComplete(isDeleted) {
           case util.Success(response) => complete(StatusCodes.OK, "Done")
           case util.Failure(exception) => {
@@ -116,9 +139,23 @@ object Main extends App with JsonSupport {
   def apmServiceRoutes = path(PathMatchers.separateOnSlashes("apm")) {
     post {
       entity(as[APMServerDetails]) { apmServerDetails =>
-        val serverDetails = aPMService.saveAPMServerDetails(apmServerDetails)
+        logger.debug(s"Executing $getClass :: saveAPMServerDetails")
+        val serverDetailsEnity = apmServerDetails.toNeo4jGraph()
+        val serverDetails = Future {
+          serverDetailsEnity match {
+            case Some(serverDetailsEntity) => {
+              apmServerDetails.fromNeo4jGraph(Some(serverDetailsEnity.get.getId))
+            }
+            case None => None
+          }
+        }
         onComplete(serverDetails) {
-          case util.Success(response) => complete(StatusCodes.OK, response)
+          case util.Success(response) => {
+            response match {
+              case Some(details) => complete(StatusCodes.OK, response)
+              case None => complete(StatusCodes.OK, "Unable to Save Server Details")
+            }
+          }
           case util.Failure(exception) => {
             logger.error(s"Unable to save the APM Server Details $exception")
             complete(StatusCodes.BadRequest, "Unable to save the Server details")
@@ -128,7 +165,9 @@ object Main extends App with JsonSupport {
     }
   } ~ path(PathMatchers.separateOnSlashes("apm")) {
     get {
-      val serverDetailsList = aPMService.getAPMServersList()
+      val serverDetailsList = Future {
+        getAPMServers().toList
+      }
       onComplete(serverDetailsList) {
         case util.Success(response) => complete(StatusCodes.OK, response)
         case util.Failure(exception) => {
@@ -141,9 +180,21 @@ object Main extends App with JsonSupport {
     serverId =>
       get {
         logger.info(s"getting into request context : $serverId")
-        val serverDetailsList = aPMService.getAPMServerUrl(serverId)
+
+        val serverDetails = APMServerDetails.fromNeo4jGraph(Some(serverId))
+        val serverDetailsList = Future {
+          serverDetails match {
+            case Some(serverDetails) => Some(serverDetails.serverUrl)
+            case None => None
+          }
+        }
         onComplete(serverDetailsList) {
-          case util.Success(response) => complete(StatusCodes.OK, response)
+          case util.Success(response) => {
+            response match {
+              case Some(detailsList) => complete(StatusCodes.OK, detailsList)
+              case None => complete(StatusCodes.BadRequest, s"Unable to get URL with given ID : $serverId")
+            }
+          }
           case util.Failure(exception) => {
             logger.error(s"Unable to get the APM Server Url $exception")
             complete(StatusCodes.BadRequest, "Unable to get the APM Server Url")
@@ -152,9 +203,27 @@ object Main extends App with JsonSupport {
       }
   } ~ path("apm" / IntNumber) {
     siteId => get {
-      val serverDetails = aPMService.getAPMServerBySite(siteId)
+      val site = Site.fromNeo4jGraph(Some(siteId))
+      val serverDetails = Future {
+        site match {
+          case Some(site) => {
+            val aPMServerDetails = getAPMServers()
+            logger.info(s"All Sever details : $aPMServerDetails")
+            val list = aPMServerDetails.filter(server => {
+              if (!server.monitoredSite.isEmpty) server.monitoredSite.get.id == site.id else false
+            })
+            logger.info(s"Filtered Server details : $list")
+            Some(list.toList)
+          }
+          case None => None
+        }
+      }
       onComplete(serverDetails) {
-        case util.Success(response) => complete(StatusCodes.OK, response)
+        case util.Success(response) =>
+          response match {
+            case Some(details) => complete(StatusCodes.OK, details)
+            case None => complete(StatusCodes.BadRequest, s"Unable to get APMServer Details with given Site ID : $siteId")
+          }
         case util.Failure(exception) =>
           logger.error(s"Unable to get the APM Server Details : ${exception.getMessage}")
           complete(StatusCodes.BadRequest, exception.getMessage)
@@ -162,10 +231,26 @@ object Main extends App with JsonSupport {
     }
   }
 
-
   val routes = appSettingServiceRoutes ~ apmServiceRoutes
   Http().bindAndHandle(routes, "localhost", 8000)
   logger.info(s"Server online at http://localhost:8000")
 
+  def getAPMServers(): mutable.MutableList[APMServerDetails] = {
+    logger.debug(s"Executing $getClass :: getAPMServers")
+    val nodes = APMServerDetails.getAllEntities()
+    logger.debug(s"Getting all entities and size is :${nodes.size}")
+    val list = mutable.MutableList.empty[APMServerDetails]
+    nodes.foreach {
+      node =>
+        val aPMServerDetails = APMServerDetails.fromNeo4jGraph(Some(node.getId))
+        aPMServerDetails match {
+          case Some(serverDetails) => list.+=(serverDetails)
+          case _ => logger.info(s"Node not found with ID: ${node.getId}")
+        }
+
+    }
+    logger.debug(s"Reurning list of APM Servers $list")
+    list
+  }
 }
 
--- src/main/scala/com/activegrid/models/PlugIn.scala
+++ src/main/scala/com/activegrid/models/PlugIn.scala
@@ -1,10 +1,13 @@
 package com.activegrid.models
 
+import akka.http.scaladsl.common.StrictForm.FileData
+
+
 /**
   * Created by nagulmeeras on 18/10/16.
   */
 case class PlugIn(override val id: Option[Long],
                   name: String,
                   active: Boolean,
-                  classLoader: Option[ClassLoader]) extends BaseEntity
+                  file: FileData) extends BaseEntity
 
