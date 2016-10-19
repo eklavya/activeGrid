573908f35cb8228e89716096bc1dd0e08c5378e0
sivag <siva.gollu@imaginea.com>
2016-10-19 20:10:37 +0530

    stacktrace included in logging statements

--- src/main/scala/com/activegrid/controllers/LogConfigUpdateController.scala
+++ src/main/scala/com/activegrid/controllers/LogConfigUpdateController.scala
@@ -3,6 +3,8 @@ package com.activegrid.controllers
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import com.activegrid.models.LogConfigUpdater
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.Future
 
@@ -13,14 +15,18 @@ class LogConfigUpdateController {
 
   val cfgUpdater = new LogConfigUpdater
 
+  val logger = Logger(LoggerFactory.getLogger(getClass.getName));
+
   val updateRQ = pathPrefix("config") {
     path("logs" / "level") {
       put {
         entity(as[String]) { level =>
           val saved: Future[String] = cfgUpdater.setLogLevel(cfgUpdater.ROOT, level)
           onComplete(saved) {
-            case util.Success(saved) =>  complete(StatusCodes.OK, "Settings saved successfully")
-            case util.Failure(saved) => complete("ERROR  WHILE UPDATING LOG LEVEL")
+            case util.Success(saved) => complete(StatusCodes.OK, "Settings saved successfully")
+            case util.Failure(ex) =>
+              logger.error("ERROR WHILE UPDATING LOG LEVEL", ex)
+              complete("ERROR  WHILE UPDATING LOG LEVEL")
           }
 
         }
@@ -34,8 +40,10 @@ class LogConfigUpdateController {
         entity(as[String]) { level =>
           val loglevel: Future[String] = cfgUpdater.getLogLevel(level)
           onComplete(loglevel) {
-            case util.Success(loglevel) =>  complete(StatusCodes.OK, "Settings saved successfully")
-            case util.Failure(loglevel) => complete("ERROR  WHILE GETTING LOG SETTINGS")
+            case util.Success(loglevel) => complete(StatusCodes.OK, "Settings saved successfully")
+            case util.Failure(ex) =>
+              logger.error("ERROR WHILE GETTING LOG SETTINGS", ex)
+              complete("ERROR  WHILE GETTING LOG SETTINGS")
           }
 
         }
