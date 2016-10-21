35434b714405bc8509a6c9c23fb51ba92a23fd80
sivag <siva.gollu@imaginea.com>
2016-10-21 15:02:20 +0530

    Remove used code and code formatting

--- src/main/scala/com/activegrid/entities/AppSettings.scala
+++ src/main/scala/com/activegrid/entities/AppSettings.scala
@@ -5,23 +5,19 @@ import com.typesafe.scalalogging.Logger
 import org.neo4j.graphdb.Node
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.Future
-
-
 case class AppSettings(settings: Map[String, String], authSettings: Map[String, String]) extends BaseEntity
 
-object AppSettings{
+object AppSettings {
   val logger = Logger(LoggerFactory.getLogger(getClass.getName))
 
-  implicit class RichAppSettings(appSettings: AppSettings) extends Neo4JRepo[AppSettings]
-  {
+  implicit class RichAppSettings(appSettings: AppSettings) extends Neo4JRepo[AppSettings] {
     override def toGraph(entity: AppSettings): Option[Node] = {
-      val maybeNode =  AppSettingsNeo4jWrapper.toNeo4jGraph(entity)
+      val maybeNode = AppSettingsNeo4jWrapper.toNeo4jGraph(entity)
       maybeNode
     }
 
-    override def fromGraph(nodeId: Long): AppSettings =  {
-       val maybeSettings = AppSettingsNeo4jWrapper.fromNeo4jGraph(0L)
+    override def fromGraph(nodeId: Long): AppSettings = {
+      val maybeSettings = AppSettingsNeo4jWrapper.fromNeo4jGraph(0L)
       maybeSettings
     }
   }
