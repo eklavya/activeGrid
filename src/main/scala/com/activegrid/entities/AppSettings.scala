d95fbdf44a3ff86a0408a818270e6953bd9f677d
sivag <siva.gollu@imaginea.com>
2016-10-21 14:45:46 +0530

    1. User of mutable map removed

--- src/main/scala/com/activegrid/entities/AppSettings.scala
+++ src/main/scala/com/activegrid/entities/AppSettings.scala
@@ -1,7 +1,32 @@
 package com.activegrid.entities
 
+import com.activegrid.neo4j.{AppSettingsNeo4jWrapper, Neo4JRepo}
+import com.typesafe.scalalogging.Logger
+import org.neo4j.graphdb.Node
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.Future
+
 
 case class AppSettings(settings: Map[String, String], authSettings: Map[String, String]) extends BaseEntity
+
+object AppSettings{
+  val logger = Logger(LoggerFactory.getLogger(getClass.getName))
+
+  implicit class RichAppSettings(appSettings: AppSettings) extends Neo4JRepo[AppSettings]
+  {
+    override def toGraph(entity: AppSettings): Option[Node] = {
+      val maybeNode =  AppSettingsNeo4jWrapper.toNeo4jGraph(entity)
+      maybeNode
+    }
+
+    override def fromGraph(nodeId: Long): AppSettings =  {
+       val maybeSettings = AppSettingsNeo4jWrapper.fromNeo4jGraph(0L)
+      maybeSettings
+    }
+  }
+
+}
 
 
 
--- src/main/scala/com/activegrid/neo4j/AppSettingsNeo4jWrapper.scala
+++ src/main/scala/com/activegrid/neo4j/AppSettingsNeo4jWrapper.scala
@@ -1,132 +1,102 @@
 package com.activegrid.neo4j
 
-import com.activegrid.entities.{AppSettings, ExecutionStatus}
+import com.activegrid.entities.AppSettings
+import com.activegrid.models.ExecutionStatus
 import com.activegrid.utils.Utils
 import com.typesafe.scalalogging.Logger
 import org.neo4j.graphdb.{Node, Relationship}
 import org.slf4j.LoggerFactory
 
-import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
 import scala.collection.immutable.HashMap
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by sivag on 6/10/16.
   */
-class AppSettingsNeo4jWrapper extends Neo4JRepo[AppSettings] with DBWrapper {
+object AppSettingsNeo4jWrapper  extends DBWrapper {
 
   val lables: HashMap[String, String] = HashMap[String, String]("GS" -> "GeneralSettings", "AS" -> "AppSettings", "AUS" -> "AuthSettings", "HAS" -> "HAS_AUTH_SETTINGS", "HGS" -> "HAS_GENERAL_SETTINGS")
   val util = new Utils()
   val logger = Logger(LoggerFactory.getLogger(getClass.getName))
 
-  override def toGraph(entity: AppSettings): Option[Node] = {
-    try {
-      withTx {
-        neo => {
-          val generalSettings = createNode(lables("GS").toString)(neo)
-          val authSettings = createNode(lables("AUS").toString)(neo)
-          util.setNodeProperties(generalSettings, entity.settings)
-          util.setNodeProperties(authSettings, entity.authSettings)
-          val appSettings = createNode(lables("AS").toString)(neo)
-          appSettings --> lables("HGS").toString --> generalSettings
-          appSettings --> lables("HAS").toString --> authSettings
-          Some(appSettings)
-        }
+  def toNeo4jGraph(entity: AppSettings): Option[Node] = {
+    withTx {
+      neo => {
+        val generalSettings = createNode(lables("GS").toString)(neo)
+        val authSettings = createNode(lables("AUS").toString)(neo)
+        util.setNodeProperties(generalSettings, entity.settings)
+        util.setNodeProperties(authSettings, entity.authSettings)
+        val appSettings = createNode(lables("AS").toString)(neo)
+        appSettings --> lables("HGS").toString --> generalSettings
+        appSettings --> lables("HAS").toString --> authSettings
+        Some(appSettings)
       }
     }
-    catch {
-      case ex: Exception => logger.error(ex.getMessage, ex)
+  }
+
+   def fromNeo4jGraph(nodeId: Long): AppSettings =  {
+     withTx {
+       neo => {
+         withTx { neo =>
+           val settingNodes = getAllNodesWithLabel(lables("AS").toString)(neo).toList
+           settingNodes.map{node => AppSettings(getSettingsByRelation(node, lables("HGS").toString), getSettingsByRelation(node, lables("HAS").toString))}.head
+         }
+       }
+     }
+   }
+
+  def updateSettings(settingsToUpdate: Map[String, String], settingsType: String): Future[ExecutionStatus] = {
+    if(settingsType.equalsIgnoreCase("AUTH_SETTINGS"))
+     updateOrDeleteSettings(settingsToUpdate,lables("HAS").toString,"UPDATE")
+    else
+      updateOrDeleteSettings(settingsToUpdate,lables("HGS").toString,"UPDATE")
+
+  }
+  def deleteSetting(settingsToDelete: Map[String, String], settingsType: String): Future[ExecutionStatus] = {
+    if(settingsType.equalsIgnoreCase("AUTH_SETTINGS"))
+      updateOrDeleteSettings(settingsToDelete,lables("HAS").toString,"DELETE")
+    else
+      updateOrDeleteSettings(settingsToDelete,lables("HGS").toString,"DELETE")
+  }
+  def getSettingsByRelation(rootNode:Node, relationName:String): Map[String,String] = {
+    val relationNode = getRelationNodeByName(rootNode,relationName)
+    relationNode match {
+      case Some(node) => node.getAllProperties.mapValues(_.toString()).toMap[String,String]
+      case None => Map.empty[String,String]
     }
-    None
   }
 
-  override def fromGraph(nodeId: Long): AppSettings = {
-    val genaralSettings = scala.collection.mutable.HashMap.empty[String, String]
-    val authSettings = scala.collection.mutable.HashMap.empty[String, String]
-      withTx {
-        neo => {
-          withTx { neo =>
-            val settingNodes = getAllNodesWithLabel(lables("AS").toString)(neo)
-            for (n <- settingNodes) {
-              logger.info(n.getAllProperties.toString)
-              val iterator = n.getRelationships().iterator()
-              while (iterator.hasNext) {
-                val relation: Relationship = iterator.next
-                logger.info("Processing " + relation.getType.name())
-                if (relation.getType.name.equalsIgnoreCase(lables("HAS").toString)) {
-                  logger.info("Processing " + relation.getType.name())
-                  relation.getEndNode.getAllProperties.asScala.foreach {
-                    case (k, v) => authSettings += (k -> v.toString)
-                  }
+  def getRelationNodeByName(rootNode:Node, relationName:String): Option[Node] = {
+    rootNode.getRelationships.filter(relation => relation.getType.name() == relationName).headOption match {
+      case Some(relationship) => Some(relationship.getEndNode)
+      case None => None
+    }
+  }
+  def updateOrDeleteSettings(settings:Map[String,String], relationName:String, updateOrDelete:String): Future[ExecutionStatus] = Future{
+    withTx {
+      neo => {
+        withTx { neo =>
+          val settingNodes = getAllNodesWithLabel(lables("AS").toString)(neo).headOption
+          settingNodes match {
+            case Some(rootNode) =>  val relationNode  = getRelationNodeByName(rootNode,relationName)
+              relationNode match {
+                case Some(dbnode) =>
+                  val todelete = if(updateOrDelete.equalsIgnoreCase("DELETE")) true else false
+                  settings.foreach {
+                  case (k, v) =>
+                    if(todelete)
+                       dbnode.removeProperty(k)
+                    else
+                      dbnode.setProperty(k,v.toString)
                 }
-                if (relation.getType.name.equalsIgnoreCase(lables("HGS").toString)) {
-                  logger.info("Processing " + relation.getType.name())
-                  relation.getEndNode.getAllProperties.asScala.foreach {
-                    case (k, v) => genaralSettings += (k -> v.toString)
-                  }
-                }
+                case None => ExecutionStatus(false)
               }
-            }
-
+            case None => ExecutionStatus(false)
           }
         }
       }
-    logger.info(genaralSettings.toMap.toString())
-    logger.info(authSettings.toMap.toString())
-    AppSettings(genaralSettings.toMap, authSettings.toMap)
-  }
-
-  def updateSettings(settingsMap: Map[String, String], relationName: String): ExecutionStatus = {
-      withTx {
-        neo => {
-          withTx { neo =>
-            // "AS" is key to "Application Settings" defined in the MAP  "labels"
-            // Fetching  nodes with  "Application Settings" Label
-            val settingNodes = getAllNodesWithLabel(lables("AS").toString)(neo)
-            for (n <- settingNodes) {
-              logger.info(n.getAllProperties.toString)
-              val iterator = n.getRelationships().iterator()
-              while (iterator.hasNext) {
-                val relation: Relationship = iterator.next
-                logger.info("Processing " + relation.getType.name())
-                if (relation.getType.name.equalsIgnoreCase(relationName)) {
-                  val node = relation.getEndNode
-                  settingsMap.foreach {
-                    case (k, v) => node.setProperty(k, v.toString)
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    ExecutionStatus(true)
-  }
-
-  def deleteSetting(settingsToDelete: Map[String, String], relationName: String): ExecutionStatus = {
-      withTx {
-        neo => {
-          withTx { neo =>
-            val settingNodes = getAllNodesWithLabel(lables.get("AS").toString)(neo)
-            for (n <- settingNodes) {
-              logger.info(s"${n.getAllProperties}")
-              val iterator = n.getRelationships().iterator()
-              while (iterator.hasNext) {
-                val relation: Relationship = iterator.next
-                if (relation.getType.name.equalsIgnoreCase(relationName)) {
-                  val node = relation.getEndNode
-                  logger.info(settingsToDelete.toString())
-                  logger.info(s" properties ${node.getAllProperties}")
-                  settingsToDelete.foreach {
-                    case (k, v) => logger.info(s"Delete ${k} and ${v}")
-                      logger.info("Is property existed..." + node.hasProperty(k.toString.trim).toString)
-                      node.removeProperty(k)
-
-                  }
-                }
-              }
-            }
-          }
-        }
     }
     ExecutionStatus(true)
   }
