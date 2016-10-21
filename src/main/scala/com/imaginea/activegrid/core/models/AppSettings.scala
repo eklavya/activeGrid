be9c76d87c68d09fbe2e806ac9277b1be25678d5
sivag <siva.gollu@imaginea.com>
2016-10-21 19:30:02 +0530

    Changed method signature

--- src/main/scala/com/imaginea/activegrid/core/models/AppSettings.scala
+++ src/main/scala/com/imaginea/activegrid/core/models/AppSettings.scala
@@ -4,20 +4,21 @@ import com.typesafe.scalalogging.Logger
 import org.neo4j.graphdb.Node
 import org.slf4j.LoggerFactory
 
-case class AppSettings(settings: Map[String, String], authSettings: Map[String, String]) extends BaseEntity
+case class AppSettings(override val id:Option[Long],settings: Map[String, String], authSettings: Map[String, String]) extends BaseEntity
 
 object AppSettings {
+
   val logger = Logger(LoggerFactory.getLogger(getClass.getName))
 
-  implicit class RichAppSettings(appSettings: AppSettings) extends Neo4JRepo[AppSettings] {
+  implicit class RichAppSettings(appSettings: AppSettings) extends Neo4JRep[AppSettings] {
     override def toGraph(entity: AppSettings): Option[Node] = {
-      val maybeNode = AppSettingsNeo4jWrapper.toNeo4jGraph(entity)
-      maybeNode
+      AppSettingsNeo4jWrapper.toNeo4jGraph(entity)
+
     }
 
     override def fromGraph(nodeId: Long): AppSettings = {
-      val maybeSettings = AppSettingsNeo4jWrapper.fromNeo4jGraph(0L)
-      maybeSettings
+       AppSettingsNeo4jWrapper.fromNeo4jGraph(0L)
+
     }
   }
 
