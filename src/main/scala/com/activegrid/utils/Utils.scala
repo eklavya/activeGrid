a5754df6a6cde5a13fcf2564521f58423b59968b
sivag <siva.gollu@imaginea.com>
2016-10-21 15:02:20 +0530

    Remove used code and code formatting

--- src/main/scala/com/activegrid/utils/Utils.scala
+++ src/main/scala/com/activegrid/utils/Utils.scala
@@ -1,6 +1,6 @@
 package com.activegrid.utils
 
-import org.neo4j.graphdb.{Node, Relationship};
+import org.neo4j.graphdb.Node
 
 
 class Utils {
@@ -10,24 +10,4 @@ class Utils {
       case (k, v) => n.setProperty(k, v);
     }
   }
-
-
-  /**
-    *
-    */
-  def validdateInput: Unit = {
-
-  }
-
-  /*  def javaMapToScalaImutableMap(node:Node): Map[String,String] = {
-      val dummyMap   = scala.collection.mutable.HashMap.empty[String,String].empty
-      val extraNodeIterator = node.getPropertyKeys.iterator();
-      while(extraNodeIterator.hasNext){
-        val key = extraNodeIterator.next();
-        val value = node.getProperty(key);
-        dummyMap += (key->value.asInstanceOf[String])
-      }
-      dummyMap.toMap;
-}*/
-
 }
