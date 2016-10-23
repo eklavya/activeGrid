3ee0911c2eafa98cd88cdf4d85ee5a1175d122f8
nagulmeeras <nagulmeera.shaik@imaginea.com>
2016-10-23 19:26:08 +0530

    APMService implementation initial commit

--- src/main/scala/com/activegrid/models/JsonSupport.scala
+++ src/main/scala/com/activegrid/models/JsonSupport.scala
@@ -9,6 +9,28 @@ import spray.json.{DefaultJsonProtocol, DeserializationException, JsString, JsVa
   * Created by nagulmeeras on 28/09/16.
   */
 trait JsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
+  val logger = LoggerFactory.getLogger(getClass)
   implicit val appSettingsFormat = jsonFormat(AppSettings.apply, "id", "settings", "authSettings")
+  implicit val siteFormat = jsonFormat(Site.apply,"id","siteName","groupBy")
+
+  implicit object apmProviderFormat extends RootJsonFormat[APMProvider]{
+    override def write(obj: APMProvider): JsValue = {
+      logger.info(s"Writing APMProvider json : ${obj.provider.toString}")
+      JsString(obj.provider.toString)
+    }
+
+    override def read(json: JsValue): APMProvider = {
+      logger.info(s"Reading json value : ${json.toString}")
+      json match {
+        case JsString(str) => APMProvider.toProvider(str)
+        case _=> throw new DeserializationException("Unable to deserialize the Provider data")
+      }
+    }
+  }
+
+  implicit val apmServerDetailsFormat = jsonFormat(APMServerDetails.apply , "id","name","serverUrl","monitoredSite","provider","headers")
+
+
+
 
 }
