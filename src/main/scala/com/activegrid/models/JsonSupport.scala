6c4ca65f79838f3016e6eaa8a06dac3be25e0c70
nagulmeeras <nagulmeera.shaik@imaginea.com>
2016-10-23 19:25:24 +0530

    Changed repository files for removing dummy objects

--- src/main/scala/com/activegrid/models/JsonSupport.scala
+++ src/main/scala/com/activegrid/models/JsonSupport.scala
@@ -1,13 +1,14 @@
 package com.activegrid.models
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import spray.json.DefaultJsonProtocol
+import org.slf4j.LoggerFactory
+import spray.json.{DefaultJsonProtocol, DeserializationException, JsString, JsValue, RootJsonFormat}
 
 
 /**
   * Created by nagulmeeras on 28/09/16.
   */
 trait JsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
-
   implicit val appSettingsFormat = jsonFormat(AppSettings.apply, "id", "settings", "authSettings")
+
 }
