8c802df41ba8f990931ee78f76a5171b08de3df6
nagulmeeras <nagulmeera.shaik@imaginea.com>
2016-10-23 19:22:07 +0530

    Changes made according to the design in neo4j repositories

--- src/main/scala/com/activegrid/models/JsonSupport.scala
+++ src/main/scala/com/activegrid/models/JsonSupport.scala
@@ -1,26 +1,13 @@
 package com.activegrid.models
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import org.joda.time.DateTime
-import org.joda.time.format.ISODateTimeFormat
-import spray.json.{DefaultJsonProtocol, JsString, JsValue, JsonFormat}
+import spray.json.DefaultJsonProtocol
 
 
 /**
   * Created by nagulmeeras on 28/09/16.
   */
 trait JsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
-  implicit val settingFormat = jsonFormat3(Setting.apply)
 
-  implicit object DateFormate extends JsonFormat[DateTime] {
-    val parser = ISODateTimeFormat.dateOptionalTimeParser()
-    override def write(obj: DateTime): JsString = JsString(ISODateTimeFormat.basicDateTime.print(obj))
-
-    override def read(json : JsValue): DateTime = json match {
-      case JsString(json) => parser.parseDateTime(json)
-      case _ => throw new Exception("Unable to Deserialize")
-    }
-  }
-
-  implicit val appSettingsFormat = jsonFormat(AppSettings ,"id","settings","authSettings")
+  implicit val appSettingsFormat = jsonFormat(AppSettings.apply, "id", "settings", "authSettings")
 }
