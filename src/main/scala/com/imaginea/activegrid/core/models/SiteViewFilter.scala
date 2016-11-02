f12341b0d3babd424c87cf0a7d698180b2c64c35
sivag <siva.gollu@imaginea.com>
2016-11-02 11:17:47 +0530

    Added filters code

---
+++ src/main/scala/com/imaginea/activegrid/core/models/SiteViewFilter.scala
@@ -0,0 +1,9 @@
+package com.imaginea.activegrid.core.models
+
+/**
+  * Created by sivag on 28/10/16.
+  */
+class SiteViewFilter extends  ViewFilter[Site]{
+  override def filterInstance(t: Site, viewLevel: Any): Site = ???
+
+}
