3e3e631d726de3c428735697f7ed7ce07695103a
sivag <siva.gollu@imaginea.com>
2016-11-22 11:54:40 +0530

    Rebase to master

--- src/main/scala/com/imaginea/Main.scala
+++ src/main/scala/com/imaginea/Main.scala
@@ -1505,7 +1505,21 @@ object Main extends App {
         }
       }
     }
-  }
+  } ~
+    path("site"/LongNumber/"policies"/Segment){
+      (siteId,policyId) => {
+        delete {
+          val maybeDelete = Future {
+            SiteManagerImpl.deletePolicy(policyId)
+          }
+          onComplete(maybeDelete){
+            case Success(executionStatus) => complete(StatusCodes.OK,executionStatus.msg)
+            case Failure(ex) => logger.info(s"Error while deleting policy $policyId",ex)
+              complete(StatusCodes.BadRequest,"Error while deleting policy")
+          }
+        }
+      }
+    }
 
 
   val route: Route = siteServices ~ userRoute ~ keyPairRoute ~ catalogRoutes ~ appSettingServiceRoutes ~
