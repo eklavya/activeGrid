10942edea17f5e2470e0565c1e0974ac775c3bb5
nagulmeeras <nagulmeera.shaik@imaginea.com>
2016-12-01 13:38:59 +0530

    Null cases are handled while getting node

--- src/main/scala/com/imaginea/activegrid/core/models/EsManager.scala
+++ src/main/scala/com/imaginea/activegrid/core/models/EsManager.scala
@@ -1,8 +1,10 @@
 package com.imaginea.activegrid.core.models
 
+import com.typesafe.scalalogging.Logger
 import org.elasticsearch.common.settings.ImmutableSettings
 import org.elasticsearch.index.query.{BoolQueryBuilder, QueryBuilders}
 import org.elasticsearch.node.NodeBuilder
+import org.slf4j.LoggerFactory
 import spray.json.RootJsonFormat
 
 import scala.collection.JavaConversions._
@@ -15,74 +17,103 @@ object EsManager {
     .put("cluster.name", "elasticsearch")
     .put("node.name", this.getClass.getProtectionDomain.getCodeSource.getLocation.toURI.getPath + "/config/names.txt")
     .build()
-  val node = NodeBuilder.nodeBuilder().local(false).settings(settings).build().start()
-  val client = node.client()
+  val mayBeNode = Option(NodeBuilder.nodeBuilder().local(false).settings(settings).build())
+    .flatMap(node => Option(node.start()))
+  val mayBeClient = mayBeNode.flatMap(node => Option(node.client()))
   val searchHitsSize = 1000
+  val logger = Logger(LoggerFactory.getLogger(getClass.getName))
 
   def shutDown(): Unit = {
-    node.stop.close()
+    mayBeNode match {
+      case Some(node) => node.stop().close()
+      case None =>
+        logger.warn("Unable to load the node")
+        throw new Exception("Unable to load the node")
+    }
+
   }
 
   def indexEntity[T <: BaseEntity](entity: T, index: String, indexType: String)(implicit formateObj: RootJsonFormat[T]): Unit = {
     entity.id.foreach { id =>
-      val v = formateObj.write(entity).toString()
-      val respo = client.prepareIndex(index.toLowerCase, indexType, id.toString).setSource(v).execute().actionGet()
+      Option(formateObj.write(entity)).foreach { jsValue =>
+        mayBeClient.foreach { client =>
+          Option(client.prepareIndex(index.toLowerCase, indexType, id.toString))
+            .flatMap(indexRequest => Option(indexRequest.setSource(jsValue.toString())))
+            .flatMap(indexRequest => Option(indexRequest.execute()))
+            .foreach(actionFuture => actionFuture.actionGet())
+        }
+      }
     }
   }
 
   def boolQuery(esSearchQuery: EsSearchQuery): List[EsSearchResponse] = {
     val index = esSearchQuery.index.toLowerCase
-    val searchRequest = client.prepareSearch(index)
-    searchRequest.addField(esSearchQuery.outputField)
-    esSearchQuery.types.foreach(searchType => searchRequest.setTypes(searchType))
-    val query = new BoolQueryBuilder
-    esSearchQuery.queryFields.foreach { queryField =>
-      val qsQuery = QueryBuilders.queryString(addWildCard(queryField.value))
-      qsQuery.field(addWildCard(queryField.key))
-      qsQuery.analyzeWildcard(true)
-      esSearchQuery.queryType match {
-        case EsQueryType.OR =>
-          query.should(qsQuery)
-        case EsQueryType.AND =>
-          query.must(qsQuery)
-      }
-    }
-    searchRequest.setQuery(query)
-    searchRequest.setSize(searchHitsSize)
-    val response = searchRequest.execute.actionGet
-    response.getHits.toList.map { hit =>
-      EsSearchResponse(hit.getType, esSearchQuery.outputField, hit.getId)
+    mayBeClient match {
+      case Some(client) => val searchRequest = client.prepareSearch(index)
+        searchRequest.addField(esSearchQuery.outputField)
+        esSearchQuery.types.foreach(searchType => searchRequest.setTypes(searchType))
+        val query = new BoolQueryBuilder
+        esSearchQuery.queryFields.foreach {
+          queryField =>
+            val qsQuery = QueryBuilders.queryString(addWildCard(queryField.value))
+            qsQuery.field(addWildCard(queryField.key))
+            qsQuery.analyzeWildcard(true)
+            esSearchQuery.queryType match {
+              case EsQueryType.OR =>
+                query.should(qsQuery)
+              case EsQueryType.AND =>
+                query.must(qsQuery)
+            }
+        }
+        searchRequest.setQuery(query)
+        searchRequest.setSize(searchHitsSize)
+        val response = searchRequest.execute.actionGet
+        response.getHits.toList.map {
+          hit =>
+            EsSearchResponse(hit.getType, esSearchQuery.outputField, hit.getId)
+        }
+      case None =>
+        logger.warn("Unable to load the client")
+        throw new Exception("Unable to load the client")
     }
   }
 
   def addWildCard(queryString: String): String = {
-    if ("_all" == queryString) queryString
-    val wildCardStr = new StringBuilder
-    if (!queryString.startsWith("*")) wildCardStr.append("*")
-    if (!queryString.endsWith("*")) wildCardStr.append(queryString).append("*")
-    wildCardStr.toString
+    if ("_all".equals(queryString)) {
+      queryString
+    } else {
+      val wildCardStr = new StringBuilder
+      if (!queryString.startsWith("*")) wildCardStr.append("*").append(queryString)
+      if (!queryString.endsWith("*")) wildCardStr.append(queryString).append("*")
+      wildCardStr.toString
+    }
   }
 
   def fieldMappings(indexName: String, mappingType: String): Set[String] = {
     val index = indexName.toLowerCase
-    val mayBeClusterState = Option(client.admin())
-      .flatMap(admin => Option(admin.cluster()))
-      .flatMap(cluster => Option(cluster.prepareState()))
-      .flatMap(state => Option(state.setFilterIndices(index)))
-      .flatMap(filter => Option(filter.execute()))
-      .flatMap(execution => Option(execution.actionGet()))
-      .flatMap(action => Option(action.getState))
-    //client.admin().cluster().prepareState().setFilterIndices(index).execute().actionGet().getState
-    val mayBeMapping = mayBeClusterState
-      .flatMap(clusterState => Option(clusterState.getMetaData))
-      .flatMap(metaData => Option(metaData.index(index)))
-      .flatMap(index => Option(index.mapping(mappingType)))
-    //getMetaData.index(index).mapping(mappingType)\
-    mayBeMapping match {
-      case Some(mapping) => flattenKeys(mapping.getSourceAsMap, mappingType)
-      case None => Set.empty[String]
+    mayBeClient match {
+      case Some(client) =>
+        val mayBeClusterState = Option(client.admin())
+          .flatMap(admin => Option(admin.cluster()))
+          .flatMap(cluster => Option(cluster.prepareState()))
+          .flatMap(state => Option(state.setFilterIndices(index)))
+          .flatMap(filter => Option(filter.execute()))
+          .flatMap(execution => Option(execution.actionGet()))
+          .flatMap(action => Option(action.getState))
+        val mayBeMapping = mayBeClusterState
+          .flatMap(clusterState => Option(clusterState.getMetaData))
+          .flatMap(metaData => Option(metaData.index(index)))
+          .flatMap(index => Option(index.mapping(mappingType)))
+        mayBeMapping match {
+          case Some(mapping) => flattenKeys(mapping.getSourceAsMap, mappingType)
+          case None =>
+            logger.warn(s"No mappings found with $mappingType")
+            Set.empty[String]
+        }
+      case None =>
+        logger.warn("Unable to load the client")
+        throw new Exception("Unable to load the client")
     }
-
   }
 
   def flattenKeys(map: java.util.Map[String, AnyRef], parent: String): Set[String] = {
