93200edc31b8fb451cca3ae8dfc2a896203cd59c
nagulmeeras <nagulmeera.shaik@imaginea.com>
2016-12-20 14:59:35 +0530

    Modified in grep command

--- src/main/scala/com/imaginea/activegrid/core/models/GrepCommand.scala
+++ src/main/scala/com/imaginea/activegrid/core/models/GrepCommand.scala
@@ -1,21 +1,35 @@
 package com.imaginea.activegrid.core.models
 
+import com.beust.jcommander.Parameter
 import com.imaginea.activegrid.core.utils.CmdExecUtils
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConversions._
 
 /**
   * Created by nagulmeeras on 12/12/16.
   */
-class GrepCommand {
-  val caseInsensitive: Boolean = false
-  val recursive: Boolean = false
-  val inputs = List.empty[String]
+class GrepCommand extends Command{
+  @Parameter(names = Array("-i"), description = "is the pattern match case insensitive")
+  var caseInsensitive: Boolean = false
+
+  @Parameter(names = Array("-r", "-R"), description = "search be done recursively")
+  var recursive: Boolean = false
+
+  @Parameter(description = "Pattern and file inputs", variableArity = true, arity = 2)
+  var inputs: java.util.List[String] = new java.util.ArrayList()
+
+  val logger = Logger(LoggerFactory.getLogger(getClass.getName))
 
   def execute(commandExecutionContext: CommandExecutionContext, inputContexts: List[Line]): List[Line] = {
     if (inputs.nonEmpty) {
       val pattern = inputs.head
-      val providerContext = inputs.size match {
-        case size if size > 1 => Some(inputs(1))
-        case _ => None
+      val providerContext = if (inputs.size > 1) {
+        Some(inputs(1))
+      } else {
+        logger.debug("More number of inputs found for Grep command")
+        None
       }
       providerContext match {
         case Some(providerCtx) =>
@@ -46,26 +60,25 @@ class GrepCommand {
         case None => inputContexts.filter { line =>
           line.values.exists(lineValue => lineValue.contains(pattern) || lineValue.matches(pattern))
         }
-        case None => List.empty[Line]
       }
     } else {
-      throw new Exception("Command execution exception")
+      logger.warn("No input parameter found")
+      throw new Exception("No input options found for command like File or Directory")
     }
   }
 
   def searchInstance(site: Site1, pattern: String): List[Line] = {
     site.instances.flatMap { instance =>
       val instanceName = instance.name
-      val dNSName = instance.publicDnsName match {
-        case Some(dnsName) => dnsName
-        case None => ""
-      }
+      val dNSName = instance.publicDnsName
       if (instanceName.contains(pattern) || instanceName.matches(pattern)
-        || dNSName.contains(pattern) || dNSName.matches(pattern)) {
+        || dNSName.exists(dnsName => dnsName.contains(pattern) || dnsName.matches(pattern))) {
         Some(Line(InstanceViewHelper.fetchInstance(instance, SUMMARY), DIRECTORY, None))
       } else {
+        logger.debug(s"No matches found in Instances with $pattern")
         None
       }
+
     }
   }
 
@@ -73,11 +86,7 @@ class GrepCommand {
     val siteNodes = Neo4jRepository.getNodesByLabel(Site1.label)
     val sites = siteNodes.flatMap(node => Site1.fromNeo4jGraph(node.getId))
     sites.flatMap { site =>
-      if (site.siteName.matches(pattern) || site.siteName.contains(pattern)) {
-        Some(Line(SiteViewFilter.fetchSite(site, SUMMARY), DIRECTORY, None))
-      } else {
-        None
-      }
+      matchWithSiteName(site, pattern)
     }
   }
 
@@ -86,12 +95,16 @@ class GrepCommand {
     val sites = siteNodes.flatMap(node => Site1.fromNeo4jGraph(node.getId))
     sites.flatMap { site =>
       val list = searchInstance(site, pattern)
-      if (site.siteName.matches(pattern) || site.siteName.contains(pattern)) {
-        Line(SiteViewFilter.fetchSite(site, SUMMARY), DIRECTORY, None) :: list
-      } else {
-        None
-      }
+      matchWithSiteName(site, pattern)
+    }
+  }
 
+  def matchWithSiteName(site: Site1, pattern: String): Option[Line] = {
+    if (site.siteName.matches(pattern) || site.siteName.contains(pattern)) {
+      Some(Line(SiteViewFilter.fetchSite(site, SUMMARY), DIRECTORY, None))
+    } else {
+      logger.debug(s"No matches found in Site with $pattern")
+      None
     }
   }
 }
