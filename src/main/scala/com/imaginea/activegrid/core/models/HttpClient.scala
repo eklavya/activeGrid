8e9e40a07760732bd6c376e5e3eb9690fffd2152
sivag <siva.gollu@imaginea.com>
2016-12-27 12:54:25 +0530

    Implementation in progress

--- src/main/scala/com/imaginea/activegrid/core/models/HttpClient.scala
+++ src/main/scala/com/imaginea/activegrid/core/models/HttpClient.scala
@@ -1,11 +1,22 @@
 package com.imaginea.activegrid.core.models
 
+import akka.http.impl.util.Util
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
+import akka.parboiled2.RuleTrace.Fail
+import com.imaginea.Main
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.Future
+import scala.util.Success
+
+
 /**
   * Created by sivag on 22/12/16.
   */
 object HttpClient {
   /**
-    *
     * @param method
     * @param url
     * @param headers
@@ -14,9 +25,25 @@ object HttpClient {
     * @return
     *         Sends a http request and process response from given "url"
     */
+  val POSTRQ = "POST"
+  val PUTRQ = "GET"
+  val logger = Logger(LoggerFactory.getLogger(HttpClient.getClass.getName))
+  val materializer = Main.materializer
+  val context = Main.executionContext
+  val http = Http(Main.system)
+
   def sendDataAsJson(method: String, url: String, headers: Map[String, String], queryParams: Map[String, String], query: APMQuery): String = {
-    "dummy response" // Returning dummy value
+    val resp = Http().singleRequest(HttpRequest(uri = url))
+    resp.onComplete(httpresponse =>
+      httpresponse.map { response =>
+        if (response.status.isSuccess()) {
+
+        }
+        else {
+        }
+      })
   }
+
 
   /**
     *
