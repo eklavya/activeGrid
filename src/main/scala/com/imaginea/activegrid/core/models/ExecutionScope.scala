ba85cec5ccebf48c4668db3d0b58e06b66e51753
sivag <siva.gollu@imaginea.com>
2017-04-04 11:09:15 +0530

    Currently unused files removed

--- src/main/scala/com/imaginea/activegrid/core/models/ExecutionScope.scala
+++
@@ -1,8 +0,0 @@
-package com.imaginea.activegrid.core.models
-
-/**
-  * Created by sivag on 19/1/17.
-  */
-case class ExecutionScope(override val id:Option[Long],scopeType: ScopeType) extends BaseEntity(id:Option[Long])
-
-
