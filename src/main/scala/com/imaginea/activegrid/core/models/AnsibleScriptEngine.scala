a3b69cec9a15a3f7c35b8284bc9ff8f9f294b1fb
sivag <siva.gollu@imaginea.com>
2017-04-04 17:34:22 +0530

    Scala warnings removed

--- src/main/scala/com/imaginea/activegrid/core/models/AnsibleScriptEngine.scala
+++ src/main/scala/com/imaginea/activegrid/core/models/AnsibleScriptEngine.scala
@@ -20,20 +20,25 @@ class AnsibleScriptEngine(val inventory: Inventory, val workflowContext: Workflo
     * @return true if process started successfully.
     */
   def executeScript(): Try[Boolean] = {
-    val workflowId: Long = workflowContext.workflow.id.get
-    val logFile = WorkflowConstants.logDirectory.concat(File.separator).concat(s"${workflowId.toString}.log")
-    val variable = inventory.getExtraVariableByName("workflow-id").getOrElse(
-      new Variable(None, "workflow-id", Some(workflowId.toString), VariableScope.toVariableScope("EXTRA"), true, true))
-    val invFilePath = WorkflowConstants.tmpDirLoc.concat(File.separator).concat(workflowId.toString)
-    val invFile = new File(invFilePath)
-    val json: String = "" //todo implementation to convert file content to json object
-    FileUtils.saveContentToFile(invFile, json)
-    if (invFile.exists()) { invFile.setExecutable(true) }
-    val command = Seq("/usr/bin/ansible-playbook", "-i", invFilePath.toString, "-e", "workflowId=" + workflowId.toString, playBook.path.toString)
-    logger.info(s"Starting playbook ${playBook.name} using inventory $json and extra variables : ${inventory.extraVariables.toString()}")
-    val lines = Process(command) lineStream_! ProcessLogger(new File(logFile))
-    lines.foreach(line => FileUtils.saveContentToFile(logFile,line))
-    logger.info(s"Workflow ${workflowId.toString} has started, Please refer logfile ${logFile}")
-    Try(true)
-  }
+    if(!workflowContext.workflow.id.isDefined) {
+      logger.error("Invalid workflow details provided")
+      Try(false)
+    } else {
+      //scala-style:off 
+      val workflowId: Long = workflowContext.workflow.id.get
+      val logFile = WorkflowConstants.logDirectory.concat(File.separator).concat(s"${workflowId.toString}.log")
+      val variable = inventory.getExtraVariableByName("workflow-id").getOrElse(
+        new Variable(None, "workflow-id", Some(workflowId.toString), VariableScope.toVariableScope("EXTRA"), true, true))
+      val invFilePath = WorkflowConstants.tmpDirLoc.concat(File.separator).concat(workflowId.toString)
+      val invFile = new File(invFilePath)
+      val json: String = "" //todo implementation to convert file content to json object
+      FileUtils.saveContentToFile(invFile, json)
+      if (invFile.exists()) { invFile.setExecutable(true) }
+      val command = Seq("/usr/bin/ansible-playbook", "-i", invFilePath.toString, "-e", "workflowId=" + workflowId.toString, playBook.path.toString)
+      logger.info(s"Starting playbook ${playBook.name} using inventory $json and extra variables : ${inventory.extraVariables.toString()}")
+      val lines = Process(command) lineStream_! ProcessLogger(new File(logFile))
+      lines.foreach(line => FileUtils.saveContentToFile(logFile, line))
+      logger.info(s"Workflow ${workflowId.toString} has started, Please refer logfile ${logFile}")
+      Try(true)
+    }
 }
