bd0d03ad9fb342ae8402c4919858a84131b5f7db
Siva Kumar <sivakumargollu@gmail.com>
2017-04-06 16:44:49 +0530

    Activegrid/Ansible script engine code added (#117)
    * Impl work in-progress
    
    * Delete and get policy end points added
    
    * Scala warning fixed
    
    * Workflow execution files added
    
    * Authenication file removed
    
    * Adding model files
    
    * Fixing scala warnings
    
    * Ansibleworkflow logic added
    
    * AnsibleScript engine code added
    
    * Currently unused files removed
    
    * Process execution added
    
    * Scala warnings removed
    
    * Scala warnings removed
    
    * Execute script code added
    
    * Error handling changed
    
    * Interchaning of try removed for 'if'
    
    * use of .get removed

--- src/main/scala/com/imaginea/activegrid/core/models/AnsibleWorkflowProcessor.scala
+++ src/main/scala/com/imaginea/activegrid/core/models/AnsibleWorkflowProcessor.scala
@@ -1,17 +1,13 @@
 package com.imaginea.activegrid.core.models
 
-import scala.collection.mutable
-import scala.concurrent.Future
-import java.util.concurrent.{ScheduledExecutorService, TimeUnit}
-
 import com.imaginea.Main
 import com.imaginea.activegrid.core.utils.ActiveGridUtils
 import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
 
+import scala.collection.mutable
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.util.Try
 
 /**
   * Created by nagulmeeras on 13/01/17.
@@ -23,9 +19,11 @@ class AnsibleWorkflowProcessor {
     workflow.id.exists(id => workflowExecutors.remove(id).isDefined)
   }
 }
+
 object AnsibleWorkflowProcessor extends WorkflowProcessor {
   val logger = Logger(LoggerFactory.getLogger(AnsibleWorkflowProcessor.getClass.getName))
-  override def executeWorkflow(workflowContext: WorkflowContext, async: Boolean): Try[Boolean] = {
+
+  override def executeWorkflow(workflowContext: WorkflowContext, async: Boolean): Unit = {
     //scalastyle:off magic.number
     implicit val system = Main.system
     implicit val materializer = Main.materializer
@@ -34,21 +32,22 @@ object AnsibleWorkflowProcessor extends WorkflowProcessor {
     val playBookRunner = new AnsiblePlayBookRunner(workflowContext)
     if (CurrentRunningWorkflows.size > WorkflowConstants.maxParlellWorkflows) {
       logger.error(s"Too many parlell workflows triggered, Maximum allowed parllel worksflows are ${WorkflowConstants.maxParlellWorkflows}")
-      throw new Exception(s"Max-limit already reached!, Allowed limit ${WorkflowConstants.maxParlellWorkflows}")
-    }
-    val workflowId = workflow.id.getOrElse(0L)
-    if(CurrentRunningWorkflows.get(workflowId).isEmpty) {
-      if (async) {
-        system.scheduler.schedule(2.seconds, 2.seconds) { playBookRunner.execute() }
-       } else {
-        playBookRunner.execute()
-      }
-      Try(true)
     } else {
-      logger.info(s"Workflow ${workflow.name} is currently running, Please try after some time.")
-      Try(false)
+      val workflowId = workflow.id.getOrElse(0L)
+      if (CurrentRunningWorkflows.get(workflowId).isEmpty) {
+        if (async) {
+          system.scheduler.schedule(2.seconds, 2.seconds) {
+            playBookRunner.execute()
+          }
+        } else {
+          playBookRunner.execute()
+        }
+      } else {
+        logger.info(s"Workflow ${workflow.name} is currently running, Please try after some time.")
+      }
     }
   }
+
   def stopWorkflow(workflow: Workflow): Unit = {
     workflow.id.foreach {
       id => CurrentRunningWorkflows.remove(id)
