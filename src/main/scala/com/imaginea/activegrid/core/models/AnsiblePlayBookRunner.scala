6f6b2ba4caa404847d42540934dbbeda4ece8bc6
sivag <siva.gollu@imaginea.com>
2017-04-06 16:40:32 +0530

    use of .get removed

--- src/main/scala/com/imaginea/activegrid/core/models/AnsiblePlayBookRunner.scala
+++ src/main/scala/com/imaginea/activegrid/core/models/AnsiblePlayBookRunner.scala
@@ -14,25 +14,29 @@ class AnsiblePlayBookRunner(workflowContext: WorkflowContext) {
 
   def execute(): Unit = {
     val workflow = workflowContext.workflow
-    val execution = workflow.execution.get
-    if (execution.inventory.isDefined && !workflow.playBooks.isEmpty) {
-      val inventory = execution.inventory.get
-      val playBook = workflow.playBooks.head
-      val engine = new AnsibleScriptEngine(inventory, workflowContext, playBook)
-      engine.executeScript() match {
-        case Failure(e) =>
-          val workflowId = WorkflowContext.get().workflow.id
-          logger.error(s"An unexpected error, Execution of workflow ($workflowId) aborted., Removig workflow from running list", e)
-          CurrentRunningWorkflows.remove(workflowId.getOrElse(0L));
-        case Success(started) =>
-          val workflowId = WorkflowContext.get().workflow.id
-          if (started) {
-            logger.error(s"Workflow ($workflowId) started successfully. Adding workflow to running list")
-            CurrentRunningWorkflows.add(workflowId.getOrElse(0L));
-          }
-          else {
-            logger.error(s" Failed to start Workflow ($workflowId), Please try again")
-          }
+    if (workflow.execution.isDefined) {
+      val execution = workflow.execution.get
+      if (execution.inventory.isDefined && !workflow.playBooks.isEmpty) {
+        val inventory = execution.inventory.get
+        val playBook = workflow.playBooks.head
+        val engine = new AnsibleScriptEngine(inventory, workflowContext, playBook)
+        engine.executeScript() match {
+          case Failure(e) =>
+            val workflowId = WorkflowContext.get().workflow.id
+            logger.error(s"An unexpected error, Execution of workflow ($workflowId) aborted., Removig workflow from running list", e)
+            CurrentRunningWorkflows.remove(workflowId.getOrElse(0L));
+          case Success(started) =>
+            val workflowId = WorkflowContext.get().workflow.id
+            if (started) {
+              logger.error(s"Workflow ($workflowId) started successfully. Adding workflow to running list")
+              CurrentRunningWorkflows.add(workflowId.getOrElse(0L));
+            }
+            else {
+              logger.error(s" Failed to start Workflow ($workflowId), Please try again")
+            }
+        }
+      } else {
+        logger.error(s"Failed to start workflow(${workflow.id.getOrElse(0L)})")
       }
     } else {
       logger.error(s"Failed to start workflow(${workflow.id.getOrElse(0L)})");
