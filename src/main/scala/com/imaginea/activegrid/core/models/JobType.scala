9d5927ff6e987935178bb8a905713f602f725ea9
sivag <siva.gollu@imaginea.com>
2017-04-07 17:36:18 +0530

    Restored deleted file

---
+++ src/main/scala/com/imaginea/activegrid/core/models/JobType.scala
@@ -0,0 +1,27 @@
+package com.imaginea.activegrid.core.models
+/**
+  * Created by sivag on 7/4/17.
+  */
+sealed trait JobType {
+  def jobType: String
+
+  override def toString: String = jobType
+}
+
+case object WorkFlow extends JobType {
+  override def jobType: String = "WORKFLOW"
+}
+
+case object ASPolicy extends JobType {
+  override def jobType: String = "POLICY"
+}
+
+case object JobType {
+  def convert(jtype: String): JobType = {
+    jtype match {
+      case "WORKFLOW" => WorkFlow
+      case "POILCY" => ASPolicy
+      case _ => ASPolicy
+    }
+  }
+}
